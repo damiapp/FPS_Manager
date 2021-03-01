373be4c56a62e9749ecd48762e2e802dad1a03a1
Nikola <nikoladamjan@live.com>
2021-03-01 19:33:17 +0100

    Mala izmena main
    mainu promenjene varijable sve sto je ostalo jeste da se dorade varijable tako da ne budu random nijedna

--- Game/main.py
+++ Game/main.py
@@ -8,16 +8,25 @@ def NN(m1, m2, w1, w2, b):
 def sigmoid(x):
     return 1/(1+numpy.exp(-x))
 
+def weight(p1,p2):
+    pass
+
 p1=Player(0.8, 0.8, 0.8, 0.8, 0.82, 0.81, 0.82, 0.86, "Srbija", "Dami", "Kodan",25)
-p2=Player(0.41, 0.42, 0.84, 0.22, 0.62, 0.71, 0.32, 0.16, "Srbija", "Veseli", "Ghost",24)
+p2=Player(0.11, 0.12, 0.14, 0.12, 0.12, 0.11, 0.12, 0.16, "Srbija", "Veseli", "Ghost",24)
 
-w1=numpy.random.randn()
-w2=numpy.random.randn()
-b=numpy.random.randn()
+
 m1=p1.get_m()
-m2=p2.get_m()
+m2=-p2.get_m()
+g1=0
+g2=0
 
-if(NN(m1,m2,w1,w2,b)>0.5):
-    print("Player2 won")
-else:
-    print("Player1 won")
+for x in range(100):
+    w1=abs(numpy.random.randn())
+    w2=abs(numpy.random.randn())
+    b=numpy.random.randn()
+    if(NN(m1,m2,w1,w2,b)>0.5):
+        g1=g1+1
+    else:
+        g2=g2+1
+print(g1)
+print(g2)
