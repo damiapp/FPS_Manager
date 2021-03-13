726a57ac9aa1f45c517e36a15224383f3b170191
Nikola <nikola.wesely.96@gmail.com>
2021-03-13 21:30:27 +0100

    Tests + Header UI
    + static tests for:
    ---- splash screen image
    ---- team logo image
    ---- team name
    ---- manager name
    ---- money, fans and tokens indicator fields (amount and image)
    + fixed the resizing of UI elements
    + added 2 indicator fields for fans and tokens

--- Game/Major Tactics FPS Manager/Assets/Scripts/Managers/MainMenuManager.cs
+++ Game/Major Tactics FPS Manager/Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
