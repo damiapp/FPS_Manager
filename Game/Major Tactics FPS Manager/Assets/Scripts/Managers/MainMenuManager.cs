fa0871693af27e5d625f255a0352c562da4475ca
Nikola <nikola.wesely.96@gmail.com>
2021-03-30 21:55:29 +0200

    Button Animation + Switch between Panels
    - button expansion animation
    - after 2s change button panel to coresponding ui panel
    TODO:
    - hook button on click effect to on button click function in main manager (PlayButton is the only hooked up one)

--- Game/Major Tactics FPS Manager/Assets/Scripts/Managers/MainMenuManager.cs
+++ Game/Major Tactics FPS Manager/Assets/Scripts/Managers/MainMenuManager.cs
@@ -5,15 +5,76 @@ using UnityEditor.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private GameObject activeButton = null;
+    private float buttonAnimationSpeed;
+    private int buttonAnimationCounter = 0;
+    private int buttonAnimationLength = 100;
 
+    public GameObject[] Panels;
+    private int activePanel = 0;
+
+    private void Start()
+    {
+        buttonAnimationSpeed = GameObject.Find("MainMenuCanvas").GetComponent<RectTransform>().rect.width/ buttonAnimationLength *2;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void onButtonClick(string buttonName)
     {
-        
+        activeButton = GameObject.Find(buttonName);
+        activeButton.transform.SetAsLastSibling();
+    }
+
+    private void Update()
+    {
+        if (activeButton != null)
+        {
+            activeButton.GetComponent<RectTransform>().sizeDelta += new Vector2(buttonAnimationSpeed, 0f);
+            if (buttonAnimationCounter > buttonAnimationLength)
+            {
+                ButtonExecution();
+                buttonAnimationCounter = 0;
+                activeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(0f, 0f);
+                activeButton = null;
+            }
+            else
+            {
+                buttonAnimationCounter++;
+            }
+        }
+    }
+
+    private void CloseActivePanel()
+    {
+        Panels[activePanel].SetActive(false);
+    }
+
+    private void ButtonExecution()
+    {
+        CloseActivePanel();
+        switch (activeButton.name)
+        {
+            case "PlayButton":
+                activePanel = 1;
+                break;
+            case "TeamButton":
+                activePanel = 2;
+                break;
+            case "CalendarButton":
+                activePanel = 3;
+                break;
+            case "StandingButton":
+                activePanel = 4;
+                break;
+            case "TradeButton":
+                activePanel = 5;
+                break;
+            case "SettingsButton":
+                activePanel = 6;
+                break;
+            default:
+                activePanel = 0;
+                break;
+        }
+        Panels[activePanel].SetActive(true);
     }
 }
